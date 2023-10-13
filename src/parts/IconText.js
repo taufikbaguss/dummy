d5aa90ea0041e66bcf8c39f79af9e1718aef1ad9
taufikbaguss <gusba1999@gmail.com>
2023-10-13 07:53:56 +0700

    first commit

---
+++ src/parts/IconText.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function IconText() {
+  return (
+    <button className="brand-text-icon" href="" type="Link">
+      Stay<span className="text-gray-900">cation.</span>
+    </button>
+  );
+}
